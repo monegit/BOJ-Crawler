a1aab2df3ba80a9e25bd5f75fff138fb3b849400
Jongwon Park <pjw970220@gmail.com>
2020-05-25 01:15:42 +0900

    clear files

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using BOJ_Crawler.Information;
+﻿// using System;
+// using BOJ_Crawler.Information;
 
-namespace BOJ_Crawler
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string user = Console.ReadLine();
-            var info = new Problem(user);
+// namespace BOJ_Crawler
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             string user = Console.ReadLine();
+//             var info = new Problem(user);
 
-            for (int i = 0; i<=10;i++)
-                Console.WriteLine(string.Format(info.problem[i]+" / "+info.language[i]+" / "+info.date[i]));
-        }
-    }
-}
+//             for (int i = 0; i<=10;i++)
+//                 Console.WriteLine(string.Format(info.problem[i]+" / "+info.language[i]+" / "+info.date[i]));
+//         }
+//     }
+// }
